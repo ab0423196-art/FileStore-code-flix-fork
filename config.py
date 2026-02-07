126e8ff099b5d6c903992b8f439f260e4b8f884a
ab0423196-art <ab0423196@gmail.com>
2026-02-07 23:30:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from logging.handlers import RotatingFileHandler
 # Bot Configuration
 LOG_FILE_NAME = "bot.log"
 PORT = '5010'
-OWNER_ID = 6497757690
+OWNER_ID = 6883111123
 
 MSG_EFFECT = 5046509860389126442
 
@@ -14,26 +14,26 @@ SHORT_TUT = "https://t.me/How_to_Download_7x/26"
 
 # Bot Configuration
 SESSION = "yato"
-TOKEN = "642712"
-API_ID = ""
-API_HASH = ""
+TOKEN = "8369058793:AAGGYl5zfPbmvjkvSSuGd2TkNKQe7eSf74Y"
+API_ID = "23264133"
+API_HASH = "945e5b76ce8550bebbeeaf5599e7ce58"
 WORKERS = 5
 
 DB_URI = "mongodb"
-DB_NAME = "yato"
+DB_NAME = "eren"
 
-FSUBS = [[-1003016571084, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
+FSUBS = [[, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
 # Database Channel (Primary)
-DB_CHANNEL =    # just put channel id dont add ""
+DB_CHANNEL = -1003785604580   # just put channel id dont add ""
 # Multiple Database Channels (can be set via bot settings)
 # DB_CHANNELS = {
-#     "-1002595092736": {"name": "Primary DB", "is_primary": True, "is_active": True},
-#     "-1001234567890": {"name": "Secondary DB", "is_primary": False, "is_active": True}
+#     "-1003785604580": {"name": "Primary DB", "is_primary": True, "is_active": True},
+#     "": {"name": "Secondary DB", "is_primary": False, "is_active": True}
 # }
 # Auto Delete Timer (seconds)
 AUTO_DEL = 300
 # Admin IDs
-ADMINS = [6497757690, 6103092779]
+ADMINS = [6883111123]
 # Bot Settings
 DISABLE_BTN = True
 PROTECT = True
